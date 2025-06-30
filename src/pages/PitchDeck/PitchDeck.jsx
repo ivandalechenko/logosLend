6f756cfc0923fa660d4c0ad8403a5351712e4d9a
Nick Loubser <84914260+NICKLOUBSER@users.noreply.github.com>
2025-06-30 17:33:20 +0200

    feat: Update PitchDeck component to embed Figma presentation
    - Changed the iframe source to embed a Figma deck instead of a Google Drive PDF.
    - Added styling for border, width, and height to improve presentation layout.
    - Exported the PitchDeck component for better modularity.

--- src/pages/PitchDeck/PitchDeck.jsx
+++ src/pages/PitchDeck/PitchDeck.jsx
@@ -2,21 +2,26 @@ import { observer } from 'mobx-react-lite';
 import './PitchDeck.scss';
 import { useEffect } from 'react';
 import loadingStore from '../../loadingStore';
-const siteUrl = import.meta.env.VITE_SITE_URL;
 
-export default observer(() => {
+const PitchDeck = observer(() => {
     useEffect(() => {
         loadingStore.setLoading(true);
     }, []);
 
     return (
         <iframe
-            src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${siteUrl}/pd.pdf#toolbar=0&scrollbar=0`}
+            src={`https://embed.figma.com/deck/DAAtwWGfuxnj5MtP0xONxN/Untitled?node-id=1-115&embed-host=share&hide-ui=1`}
             className='PitchDeck'
             onLoad={() => { loadingStore.setLoading(false) }}
             style={{
                 backgroundColor: 'transparent',
+                border: '1px solid rgba(0, 0, 0, 0.1)',
+                width: '100%',
+                height: '90%'
             }}
+            allowFullScreen
         ></iframe>
     );
 });
+
+export default PitchDeck;
